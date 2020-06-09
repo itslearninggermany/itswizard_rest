8ea6952f52341ad9c9268e6b6c1fc67526c17b65
nordmanii <thomas.nordmann@itslearning.com>
2020-06-09 22:13:49 +0200

    full renew

--- rest.go
+++ rest.go
@@ -173,6 +173,8 @@ func NewSession(endpoint, username, password string) (session RestSession, err e
 	}
 
 	session.token = responseStruct.Message.Jwt
+	//	session.token = "pPAGRyh8B-UCi-m7D5QSevDeRrZsmXGY1bjKPGoOiPmH7ffxXXw_DedA62Zyu6oraaoTR79LdGQiQJS1IdNwvMA0xtWq5Y9jlgLLyTvzy25fdqzDqHnUlPi8kfVywMcesOQBrKArlHbqkt1RSEcXtqGC-jfG9In4Jzz0yeFKKZgDVN_DxLZWWa54u6F2Fy_Mkr6gpOQcSvJDtVU6fPXy2xS8RnnT6t_3wfxD0erG6NnGmxl118WfC-iCLRPFvJWPKbuCrSowfZP5m9edy7ggQ91mGC3RcY5Rnmdfqi0L5drNeq2hHJM6QoxNPuzcoWTlSpCVrEicVur8VlJla9-VHKP4xhWxHAS0i_0zqCIRnYBnDkIh25FAy3ieLUTZ3kh63kiqvAZHTOIPE-baUe1D54g6bMW8_kd9B0ZopvHaBzckijtL0rnBDdUpAMXLtzItgkxNzHCu2MvIAv1dQo8NT7NT81yi8nu-JOM6idxHIvzxjxtQsAQAyaUHYU_RQCaAlX7uRe_cE3A_EeYp-AtryjFzr9x6eqSi0_CnCU-hQwGjfFpHc3qaxzjYoMOABr6HiNVNSao9UZM28NCK2EakcTD52woQNcXg2tpDYGxMzpk3ZdOWLSP9VNs4CTukBPkwVmAY5MPZEwOrPts7vmlEsRbkaKv4apMv99IBDDHTZEH4BKqlCeBE_NgD52v-tVxao6xUxF9Jv5-cKQzP42pl9veQE9yCy5ahLvthMH9j_wEj6Gil0jnzvY4CWakD1HJKIbXonN7TsqD-NToSpP3POfyBy8Rhm5puVP___hsi6HOapbte81cC5acS1wm9Xho1RTEkYxnqLaES3OF6y1JYLxFhXZdE9APSrOtRYvHYVJAf42O-m0eN9n0FS-OH9vXZqLayjRCHqcCRm1i_eLpjozpHxz-QC1C_NctrBO0xYnww4dlsA1AX98qO_S1enb8Owo2A92K5BWHcI6es3Ul-Tk2kW_G2p8iHkYG5H05Ll3le7KysIrRzEYqgOJ3iFEcM5GuWxE93PDoRRvzbP-fSa_fcgi_upiGq5FXXoA2G7TG8v07WPzzKYXAUuhA9hIY="
+	//	session.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBZG1pbiI6ZmFsc2UsIkF1dGhlbnRpY2F0ZWQiOnRydWUsIkVtYWlsIjoidGhvbWFzQG1mYXNmLmRlIiwiRmlyc3RBdXRoZW50aWNhdGlvbiI6dHJ1ZSwiRmlyc3RuYW1lIjoic3VwcG9ydCIsIklQIjoiMTcyLjMxLjE4LjE5MCIsIkluZm9ybWF0aW9uIjoiLS0iLCJJbnN0aXR1dGlvbiI6IlNjaHVsZW4gZGVyIExhbmRlc2hhdXB0c3RhZHQgRMO8c3NlbGRvcmYiLCJJbnN0aXR1dGlvbklEIjoyMywiTGFzdG5hbWUiOiIxMDAzNjUiLCJNb2JpbGUiOiI0OTE1MjI1NSIsIk9yZ2FuaXNhdGlvbklEIjoxMzIsIlNjaG9vbCI6IjEwMDM2NSBWb2xrZXIgUm9zaW4gU2NodWxlIiwiVHdvRmFjIjpmYWxzZSwiVXNlcklEIjoxMjgsIlVzZXJuYW1lIjoiMTAwMzY1IiwiZXhwIjoxNTg3NzMyMjQxLCJpYXQiOjE1ODc3Mjg2NDF9.VThUmV3KuUsjIioRaMB7KRBsAe7Hi5TvT2ykcxpNvrc"
 	session.endpoint = endpoint
 	return session, nil
 }
@@ -205,6 +207,9 @@ func (p *RestSession) Me() (user User, err error) {
 		return user, err
 	}
 
+	if string(body) == "Token is expired" {
+		return user, errors.New(string(body))
+	}
 	var meResponse MeResponse
 	err = json.Unmarshal(body, &meResponse)
 	if err != nil {
@@ -238,7 +243,6 @@ func (p *RestSession) SendDataFromUnivention(filename string, data []byte) (send
 	if err != nil {
 		return sendData, err
 	}
-
 	req, err := http.NewRequest("POST", p.endpoint+"/univention", bytes.NewBuffer(b))
 	if err != nil {
 		return sendData, err
@@ -255,13 +259,18 @@ func (p *RestSession) SendDataFromUnivention(filename string, data []byte) (send
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
+
 	if err != nil {
 		return sendData, err
 	}
 
-	err = json.Unmarshal(body, &sendData)
-	if err != nil {
-		return sendData, err
+	if string(body) == "Token is expired" {
+		sendData.Error = errors.New(string(body))
+	} else {
+		err := json.Unmarshal(body, &sendData)
+		if err != nil {
+			return sendData, err
+		}
 	}
 
 	return sendData, err
