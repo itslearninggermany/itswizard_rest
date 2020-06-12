7b02d9e7db752cb9d824475452668e8516a37692
nordmanii <thomas.nordmann@itslearning.com>
2020-06-12 20:39:28 +0200

    debug

--- univention.go
+++ univention.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"errors"
-	"github.com/itslearninggermany/itswizard_aes"
 	"github.com/jinzhu/gorm"
 	"io/ioutil"
 	"net/http"
@@ -58,22 +57,10 @@ const SendAesKeyFromUnivnetionApi = "/univention/aeskey"
 /*
 Send AES-Key to itslearning
 */
-func (p *RestSession) SendAesKeyFromUnivention(username string) (string, []byte, error) {
-	aes, err := itswizard_aes.NewAes()
+func (p *RestSession) SendAesKeyFromUnivention(aesKeyAsString string) (string, error) {
+	req, err := http.NewRequest("POST", p.Endpoint+SendAesKeyFromUnivnetionApi, bytes.NewBuffer([]byte(aesKeyAsString)))
 	if err != nil {
-		return "", []byte(""), err
-	}
-	o := SendAesKeyFromUniventionRequest{
-		Username: username,
-		Key:      aes.GetAesKey(),
-	}
-	b, err := json.Marshal(o)
-	if err != nil {
-		return "", []byte(""), err
-	}
-	req, err := http.NewRequest("POST", p.Endpoint+SendAesKeyFromUnivnetionApi, bytes.NewBuffer(b))
-	if err != nil {
-		return "", []byte(""), err
+		return "", err
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", p.Token)
@@ -83,25 +70,25 @@ func (p *RestSession) SendAesKeyFromUnivention(username string) (string, []byte,
 	client := &http.Client{Transport: tr}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", []byte(""), err
+		return "", err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", []byte(""), err
+		return "", err
 	}
 
 	if string(body) == "Token is expired" {
-		return string(body), []byte(""), errors.New(string(body))
+		return string(body), errors.New(string(body))
 	}
 	if string(body) == "AESKey stored" {
-		return string(body), aes.GetAesKey(), nil
+		return string(body), nil
 	}
 	if string(body) == "AESKey updated" {
-		return string(body), aes.GetAesKey(), nil
+		return string(body), nil
 	}
 
-	return "", []byte(""), errors.New(string(body))
+	return "", errors.New(string(body))
 }
 
 /*
